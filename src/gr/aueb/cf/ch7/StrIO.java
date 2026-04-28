5832bdc4e2dfbfc20d0613ab0264bded69e89117
George Peppas <gpeppas@ajpsoft.gr>
2026-04-28 20:03:50 +0300

    Add new files (strings)

---
+++ src/gr/aueb/cf/ch7/StrIO.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch7;
+
+import java.util.Scanner;
+
+public class StrIO {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str1 = "";
+        String str2 = "";
+
+        System.out.println("Παρακαλώ εισάγετε δύο strings: ");
+        str1 = in.next(); //διαβάζει μέχρι το πρώτο whitespace
+        str2 = in.nextLine(); // διαβάζει μέχει να βρει newline
+    }
+}
