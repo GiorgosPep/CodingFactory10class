e1a18e05948b8b10fb2b187c245ebd493b9505ec
gpeppas <gpeppas@ajpsoft.gr>
2026-06-01 17:15:06 +0300

    new classes Student/Main

---
+++ src/gr/aueb/cf/ch11/Student.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch11;
+
+/**
+ *
+ * Απλή κλάση με τρία πεδία (fields),
+ * απλών και σύνθετων τύπων
+ */
+public class Student {
+    int id;
+    String firstName;
+    String lastname;
+}
