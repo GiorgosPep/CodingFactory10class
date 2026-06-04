5d0f1cc4c8c8469ba750c2af291539fb1021d981
gpeppas <gpeppas@ajpsoft.gr>
2026-06-04 20:24:53 +0300

    Add Point and FlexibleNode classes with equals/hashCode implementation and generic value handling

---
+++ src/gr/aueb/cf/ch16/equals_hashcode/Main.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch16.equals_hashcode;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(1, 2);
+        Point p3 = new Point(2, 3);
+
+        System.out.println(p1.equals(p2));  //true
+        System.out.println(p1.equals(p3));  //false
+    }
+}
