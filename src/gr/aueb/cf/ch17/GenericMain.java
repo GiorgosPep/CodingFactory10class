61a9b6ad9e06531efcb92ab6ce7465410f0aff9b
gpeppas <gpeppas@ajpsoft.gr>
2026-06-04 20:52:08 +0300

    Add GenericNode, GenericMethods, and GenericMain classes for generic data handling

---
+++ src/gr/aueb/cf/ch17/GenericMain.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch17;
+
+public class GenericMain {
+
+    public static void main(String[] args) {
+        GenericNode<String> strNode = new GenericNode<>();
+        GenericNode<Double> doubleNode = new GenericNode<>();
+        var intNode = new GenericNode<Integer>();
+
+        strNode.setValue("Coding");
+        doubleNode.setValue(3.14);
+        intNode.setValue(42);
+
+        String str = strNode.getValue();
+    }
+}
